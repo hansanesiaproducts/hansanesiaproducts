ae3ca9d30e1da810d50efca3cf897ac795a3173e
Albert <albertbarnabas10@gmail.com>
2022-07-21 14:44:11 +0700

    revisi

--- src/Pages/ContactPage/components/ContactMain/ContactMain.jsx
+++ src/Pages/ContactPage/components/ContactMain/ContactMain.jsx
@@ -1,7 +1,7 @@
 import classes from "./ContactMain.module.css";
 import email from "../../../../assets/icons/email.png";
 import phone from "../../../../assets/icons/phone.png";
-import wi from "../../../../assets/company/wi.jpg";
+import wi from "../../../../assets/company/wilargo.jpg";
 import companyLogo from "../../../../assets/company/companyLogo.png";
 
 const ContactMain = () => {
@@ -13,21 +13,24 @@ const ContactMain = () => {
           <div className={classes.addressContainer}>
             <img src={companyLogo} alt="logo hansanesia" />
             <div className={classes.contactforLogo}>
-            <div className={classes.contactUsWrapper}>
-              <h4>PT. Wilargo Inti Manunggal</h4>
-              <address>
-                Villa Melati Mas Blok H1 No. 26, <br />
-                Jelupang, Serpong Utara, Tangerang Selatan
-              </address>
-              <address>
-                Jl. Asem Pendek No : 88 Cirebon, West Java, Indonesia, <br />
-                Jl. Soekarno Soekarno Hatta No. 17 , No. 17 , <br />
-                Senenan Senenan , Tahunan Tahunan , Jepara , Jawa Tengah 59426
-              </address>
-            </div>
-            <div className={classes.contactUsWrapperImage}>
-              <img src={wi} alt="" />
-            </div>
+              <div className={classes.contactUsWrapper}>
+                <div className={classes.contactUsWrapperImage}>
+                  <img src={wi} alt="" />
+                </div>
+                <div className={classes.contactAddress}>
+                  <h4>PT. Wilargo Inti Manunggal</h4>
+                  <address>
+                    Villa Melati Mas Blok H1 No. 26, <br />
+                    Jelupang, Serpong Utara, Tangerang Selatan
+                  </address>
+                  <h6>Factory :</h6>
+                  <address>
+                    Jl. Asem Pendek No : 88 Cirebon, West Java, Indonesia, <br />
+                    Jl. Soekarno Soekarno Hatta No. 17 , No. 17 , <br />
+                    Senenan Senenan , Tahunan Tahunan , Jepara , Jawa Tengah 59426
+                  </address>
+                </div>
+              </div>
             </div>
           </div>
           <div className={classes.socialsContainer}>
